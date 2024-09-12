a2b1ebdf9b8d1455d96433220ad219095685ca29
tzeyi <tytiong2@gmail.com>
2024-09-12 19:41:14 -0400

    first commit

---
+++ bluehire-frontend/bluehire_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  bluehire_frontendApp.swift
+//  bluehire-frontend
+//
+//  Created by Tiong Tze Yi on 12/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct bluehire_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
