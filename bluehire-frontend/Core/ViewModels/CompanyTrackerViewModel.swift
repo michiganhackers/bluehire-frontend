f1ee042121dae69f64efc533d27754fbeaa731e2
tzeyi <tytiong2@gmail.com>
2024-09-28 01:15:27 -0400

    Implemented View for CompanyTrackerView

---
+++ bluehire-frontend/Core/ViewModels/CompanyTrackerViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CompanyTrackerViewModel.swift
+//  bluehire-frontend
+//
+//  Created by Tiong Tze Yi on 28/09/2024.
+//
+
+import Foundation
--- bluehire-frontend/bluehire_frontendApp.swift
+++ bluehire-frontend/bluehire_frontendApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct bluehire_frontendApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                RootView()
+            }
         }
     }
 }
